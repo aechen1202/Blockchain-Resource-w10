882292e99d7f8173e1ad25a8a4bd722bbccb28e7
artchen <aechen1202@hotmail.com>
2023-11-17 16:56:29 +0800

    finish 2

--- section3/SimpleSwap/contracts/SimpleSwap.sol
+++ section3/SimpleSwap/contracts/SimpleSwap.sol
@@ -14,10 +14,6 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
     address public tokenB_;
     uint256 public reserveA_;
     uint256 public reserveB_;
-    uint public constant MINIMUM_LIQUIDITY = 10**3;
-    uint32  private blockTimestampLast;
-    uint public price0CumulativeLast;
-    uint public price1CumulativeLast;
 
     constructor(address _tokenA, address _tokenB) ERC20("LP TOKEN", "LP") {
         require(isContract(_tokenA),"SimpleSwap: TOKENA_IS_NOT_CONTRACT");
@@ -43,10 +39,11 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         require(tokenIn==tokenA_ || tokenIn==tokenB_ , "SimpleSwap: INVALID_TOKEN_IN");
         require(tokenOut==tokenA_ || tokenOut==tokenB_ , "SimpleSwap: INVALID_TOKEN_OUT");
         require(tokenIn!=tokenOut , "SimpleSwap: IDENTICAL_ADDRESS");
+
+        //get amountOut
         uint256 reserveIn = tokenIn == tokenA_ ? reserveA_ : reserveB_;
         uint256 reserveOut = tokenOut == tokenA_ ? reserveA_ : reserveB_;
-        
-        amountOut = amountIn * reserveOut / (reserveIn + amountIn);
+        amountOut = (amountIn * reserveOut) / (reserveIn + amountIn);
         
         IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
         require(amountIn > 0 || amountOut > 0, 'SimpleSwap: INSUFFICIENT_INPUT_AMOUNT');
@@ -134,10 +131,10 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
     }
 
      // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
-    function quote(uint256 amountA, uint256 reserveA, uint256 reserveB) internal pure returns (uint256 amountB) {
-        require(amountA > 0, 'UniswapV2Library: INSUFFICIENT_AMOUNT');
-        require(reserveA > 0 && reserveB > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
-        amountB = (amountA*reserveB) / reserveA;
+    function quote(uint256 amount0, uint256 reserve0, uint256 reserve1) internal pure returns (uint256 amount1) {
+        require(amount0 > 0, 'SimpleSwap: INSUFFICIENT_AMOUNT');
+        require(reserve0 > 0 && reserve1 > 0, 'SimpleSwap: INSUFFICIENT_LIQUIDITY');
+        amount1 = (amount0 * reserve1) / reserve0;
     }
 
     // this low-level function should be called from a contract which performs important safety checks
@@ -150,24 +147,19 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         uint256 _totalSupply = totalSupply(); // gas savings, must be defined here since totalSupply can update in _mintFee
         if (_totalSupply == 0) {
             liquidity = Math.sqrt(amountA * amountB);
-           //_mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
         } else {
             liquidity = Math.min(amountA * _totalSupply / reserveA_, amountB*(_totalSupply) / reserveB_);
         }
-        require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
+        require(liquidity > 0, 'SimpleSwap: INSUFFICIENT_LIQUIDITY_MINTED');
         _mint(to, liquidity);
 
         _update(balanceA, balanceB);
-        //if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
-        //emit Mint(msg.sender, amount0, amount1);
     }
 
         // this low-level function should be called from a contract which performs important safety checks
     function burn(address to,uint256 liquidity) internal returns (uint amountA, uint amountB) {
         uint256 balanceA = IERC20(tokenA_).balanceOf(address(this));
         uint256 balanceB = IERC20(tokenB_).balanceOf(address(this));
-        //uint256 liquidity = balanceOf(address(this));
-
         uint256 _totalSupply = totalSupply(); // gas savings, must be defined here since totalSupply can update in _mintFee
         amountA = liquidity * balanceA / _totalSupply; // using balances ensures pro-rata distribution
         amountB = liquidity * balanceB / _totalSupply; // using balances ensures pro-rata distribution
@@ -179,26 +171,12 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         balanceB = IERC20(tokenB_).balanceOf(address(this));
 
         _update(balanceA, balanceB);
-        //if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
-        //emit Burn(msg.sender, amount0, amount1, to);
     }
 
      // update reserves and, on the first call per block, price accumulators
     function _update(uint256 balanceA, uint256 balanceB) private {
-        //require(balanceA <= uint256(-1) && balanceB <= uint256(-1), 'UniswapV2: OVERFLOW');
-        uint32 blockTimestamp = uint32(block.timestamp % 2**32);
-        uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
-        if (timeElapsed > 0 && reserveA_ != 0 && reserveB_ != 0) {
-            // * never overflows, and + overflow is desired
-            price0CumulativeLast += (reserveB_/reserveA_)*timeElapsed;
-            price1CumulativeLast += (reserveA_/reserveB_)*timeElapsed;
-            //price0CumulativeLast += uint(UQ112x112.encode(reserveB_).uqdiv(reserveA_)) * timeElapsed;
-            //price1CumulativeLast += uint(UQ112x112.encode(reserveA_).uqdiv(reserveB_)) * timeElapsed;
-        }
         reserveA_ = uint256(balanceA);
         reserveB_ = uint256(balanceB);
-        blockTimestampLast = blockTimestamp;
-        //emit Sync(reserveA_, reserveB_);1
     }
 
     function isContract(address addr) internal view returns (bool) {
